0d7937cc548b488365a6a1969082f3e95aa15853
Keisha <keisha@example.com>
2026-05-31 00:32:14 -0500

    Add crash logger to debug 503 error

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+process.on('uncaughtException', (err) => {
+  fs.writeFileSync(__dirname + '/crash.log', err.stack);
+});
+process.on('unhandledRejection', (err) => {
+  fs.writeFileSync(__dirname + '/crash.log', err.stack);
+});
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -12,6 +19,15 @@ app.use(express.json());
 // Basic health check endpoint
 app.get('/api/health', (req, res) => {
   res.status(200).json({ status: 'ok', message: 'EVOBRAND API is running' });
+});
+
+app.get('/api/crash', (req, res) => {
+  try {
+    const log = fs.readFileSync(__dirname + '/crash.log', 'utf8');
+    res.type('text/plain').send(log);
+  } catch (err) {
+    res.status(200).send('No crash log found. App is running fine! ' + err.message);
+  }
 });
 
 // Import Routes
